419e1f778f980910ad2d2ead97177972de2d769b
Berlin <50ta2012@gmail.com>
2022-08-28 13:05:22 +0800

    語言切換選單

--- src/web/Header.js
+++ src/web/Header.js
@@ -1,5 +1,6 @@
 import "./css/header.css"
 import personSvg from "./image/person.svg"
+import globeSvg from "./image/globe.svg"
 
 export default function Header() {
   return (
@@ -8,11 +9,24 @@ export default function Header() {
       <div className="header-title">東南亞車牌辨識系統</div>
       <div className="header-feature">
         <div className="header-dropdown">
-          <img src={personSvg} alt="" onClick={() => personDropdown()} />
-          <div className="dropdown-content">
-            <a href="/profile">個人資料</a>
-            <hr />
-            <a href="/logout">登出</a>
+
+          <div className="icon-container">
+            <div className="globe">
+              <img src={globeSvg} alt="" onClick={() => iconDropdown(".globe")}></img>
+              <div className="dropdown-content">
+                <div>English</div>
+                <div>中文 (繁)</div>
+              </div>
+            </div>
+
+            <div className="person">
+              <img src={personSvg} alt="" onClick={() => iconDropdown(".person")}/>
+              <div className="dropdown-content">
+                <a href="/profile">個人資料</a>
+                <hr />
+                <a href="/logout">登出</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -22,19 +36,25 @@ export default function Header() {
 
 /* 下拉式選單機制 */
 // 頭像按鈕
-function personDropdown(){
-  const dropContentEle = document.querySelector(".root-container .header-dropdown .dropdown-content");
+function iconDropdown(target) {
+  const dropContentEle = document.querySelector(`.root-container .header-dropdown ${target} .dropdown-content`);
   const style = window.getComputedStyle(dropContentEle);
 
   style.getPropertyValue("display") === "none" ? dropContentEle.style.display = "block" : dropContentEle.style.display = "none";
 }
 // 監看點擊 (除了頭像按鈕之外的區域) 隱藏下拉選單
 document.addEventListener('click', (event) => {
-  const personImgEle = document.querySelector(".root-container .header-dropdown img");
-  const dropContentEle = document.querySelector(".root-container .header-dropdown .dropdown-content");
+  dropContentEvent(".person", event);
+  dropContentEvent(".globe", event);
+});
+
+function dropContentEvent(target, event){
+  const imgEle = document.querySelector(`.root-container .header-dropdown ${target} img`);
+  const dropContentEle = document.querySelector(`.root-container .header-dropdown ${target} .dropdown-content`);
+
   const style = window.getComputedStyle(dropContentEle);
 
-  if (style.getPropertyValue("display") === "block" && !personImgEle.contains(event.target) && !dropContentEle.contains(event.target)) {
+  if (style.getPropertyValue("display") === "block" && !imgEle.contains(event.target) && !dropContentEle.contains(event.target)) {
     dropContentEle.style.display = "none";
   }
-});
+}
