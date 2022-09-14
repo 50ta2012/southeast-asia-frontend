9d2774419596fc5a4bc738bb3ca2b11f7a01aa08
owlsinya <owlsinya@gmail.com>
2022-09-14 14:59:03 +0800

    精簡化ShowImg code & update Modal show

--- src/lpr/ShowImage.js
+++ src/lpr/ShowImage.js
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
-import ShowLargeImage from "./ShowLargeImage";
 import "./ShowImage.css";
+import { FormattedMessage } from "react-intl";
+import { Modal } from "antd";
+import "antd/dist/antd.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function ShowImage({ data, setData, index }) {
-  const [showImg, setShowImg] = useState(false);
+export default function ShowImage({ data, index }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   let imgsrc = "https://twowayiot.com" + data[index].imagePath;
   return (
     <div>
-      <img
-        className="img-grid"
-        src={`https://twowayiot.com${data[index].imagePath}`}
-        alt=""
-        onClick={() => {
-          setShowImg(true);
-        }}
-      />
-      {showImg && (
-        <ShowLargeImage key={index} setShowImg={setShowImg} img={imgsrc} />
-      )}
+      <img className="img-grid" src={imgsrc} alt="" onClick={showModal} />
+      <Modal
+        title={<FormattedMessage id="Img-btn" />}
+        centered
+        width="70%"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <img src={imgsrc} width="100%" alt="" />
+      </Modal>
     </div>
   );
 }
