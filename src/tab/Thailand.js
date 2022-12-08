2cfa1ad41e94b16c75aa6261e4f2a0f716902b26
Berlin <50ta2012@gmail.com>
2022-12-08 13:16:35 +0800

    泰國車辨不用 cors origin 認證

--- src/tab/Thailand.js
+++ src/tab/Thailand.js
@@ -2,13 +2,6 @@ import React from 'react';
 
 import { FormattedMessage, useIntl } from 'react-intl';
 
-function addOrigin(){
-	const host = 'twowayiot.com';
-
-	if (window.location.host === host) return `&origin=https://${host}`;
-	return '';
-}
-
 function ThaiTitle() {
 	// 獨立 Component，避免 Re-render
 	// 這是因為 useIntl() 只能在 Component 裡使用，才需要這種額外處理。
@@ -24,7 +17,7 @@ export default function Thailand() {
 				<FormattedMessage id="thailand-title"></FormattedMessage>
 			</div>
 			<div style={{display: "flex", justifyContent: "center"}}>
-				<iframe title='thailand-lpr-demo' width="1120" height="630" src={`https://www.youtube.com/embed/tgbNymZ7vqY${addOrigin()}`} />
+				<iframe title='thailand-lpr-demo' width="1120" height="630" src={`https://www.youtube.com/embed/tgbNymZ7vqY`} />
 			</div>
 		</>
 	);
