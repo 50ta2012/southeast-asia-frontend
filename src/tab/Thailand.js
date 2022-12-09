5e3f5d00f6d78d34017af709dfeb3c7604dfde9d
Berlin <50ta2012@gmail.com>
2022-12-09 12:08:40 +0800

    泰國車辨展示圖

--- src/tab/Thailand.js
+++ src/tab/Thailand.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { FormattedMessage, useIntl } from 'react-intl';
+import thaiDemoImage from '../thai/img/demo_001.jpg';
 
 function ThaiTitle() {
 	// 獨立 Component，避免 Re-render
@@ -17,7 +18,8 @@ export default function Thailand() {
 				<FormattedMessage id="thailand-title"></FormattedMessage>
 			</div>
 			<div style={{display: "flex", justifyContent: "center"}}>
-				<iframe title='thailand-lpr-demo' width="1120" height="630" src={`https://www.youtube.com/embed/tgbNymZ7vqY`} />
+				{/* <iframe title='thailand-lpr-demo' width="1120" height="630" src={`https://www.youtube.com/embed/tgbNymZ7vqY`} /> */}
+				<img src={thaiDemoImage} alt={"Thailand Lpr Demo"} height="630"/>
 			</div>
 		</>
 	);
