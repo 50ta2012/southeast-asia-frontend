f61e9b590662e233140cf37518b0c56f49c1aceb
Berlin <50ta2012@gmail.com>
2022-12-09 17:25:21 +0800

    泰國車辨展示圖2

--- src/tab/Thailand.js
+++ src/tab/Thailand.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { FormattedMessage, useIntl } from 'react-intl';
 import thaiDemoImage from '../thai/img/demo_001.jpg';
+import image2 from '../thai/img/demo_002.jpg';
 
 function ThaiTitle() {
 	// 獨立 Component，避免 Re-render
@@ -17,9 +18,10 @@ export default function Thailand() {
 			<div className="main-title">
 				<FormattedMessage id="thailand-title"></FormattedMessage>
 			</div>
-			<div style={{display: "flex", justifyContent: "center"}}>
+			<div style={{display: "grid", gridTemplateColumns: "auto auto"}}>
 				{/* <iframe title='thailand-lpr-demo' width="1120" height="630" src={`https://www.youtube.com/embed/tgbNymZ7vqY`} /> */}
-				<img src={thaiDemoImage} alt={"Thailand Lpr Demo"} height="630"/>
+				<img src={thaiDemoImage} alt={"demo1"} height="630"/>
+				<img src={image2} alt={"demo2"} height="630"/>
 			</div>
 		</>
 	);
